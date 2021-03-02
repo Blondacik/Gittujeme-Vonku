263c250ab72aba6a6f0abe5d7ef739815e93bcbb
David <david1.savrnoch@gmail.com>
2021-03-02 18:59:32 +0100

    haha

---
+++ src/algace.java
@@ -0,0 +1,13 @@
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.Stack;
+
+public class algace {
+    public static void main(String[] args) {
+
+        }
+
+    }
+
+
