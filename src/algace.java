f31d21b9d8c22908eae5aff63579499d4c0a1351
Blondacik <david1.savrnoch@gmail.com>
2021-03-02 21:47:54 +0100

    pridali sme kódik stromčeka charov

--- src/algace.java
+++ src/algace.java
@@ -3,11 +3,31 @@ import java.util.Arrays;
 import java.util.Queue;
 import java.util.Stack;
 
+//pridame do gitu trápny kódik
+
 public class algace {
     public static void main(String[] args) {
-
+        char pismenko = 'A';
+        char pismenko2 = 'E';
+        for (int i = 0; i < ('E' - 'A' + 1 ); i++){
+            for (int j = 0; j <= i ; j++){
+                System.out.print(pismenko + " ");
+            }
+            pismenko++;
+            System.out.println();
         }
 
+        for (int i = 4; i > ('A' - 'E' + 3); --i){
+            for (int j = 0; j<= i ; j++) {
+                System.out.print(pismenko2 + " ");
+            }
+            pismenko2--;
+            System.out.println();
+        }
     }
+}
+
+
+
 
 
